dece5ba8b51c57bb52376baa4e62e640346770b2
rambuild <276822603qq.com>
2019-12-10 22:49:25 +0800

    12-10 22:50

--- src/components/goods/goodsParams.vue
+++ src/components/goods/goodsParams.vue
@@ -8,7 +8,12 @@
         </el-breadcrumb>
         <!-- 卡片区域 -->
         <el-card class="box-card">
-            <p style='color:rgb(234,162,58);padding:0 0 10px 20px;'><i class="el-icon-warning"></i> 注意：只允许为第三级分类设置相关参数！</p>
+            <el-alert
+                title="注意：只允许为第三级分类设置相关参数！"
+                type="warning"
+                show-icon
+                :closable="false">
+            </el-alert>
             <p>
                 <span style="font-size:20px;">选择商品分类：</span> 
                 <!-- 级联选择器 -->
@@ -22,29 +27,93 @@
                      ref='casRef'>
                 </el-cascader>
             </p>
-            <el-tabs :tab-position="tabPosition" style="height: 200px;">
-                <el-tab-pane label="用户管理">
-                    <el-button type="primary" class='addCatg' @click='addCatgBtn' style='margin:5px 0 20px 0'>添加参数</el-button>
-                    <template>
-                        <el-table :data="paramsList" border stripe style="width: 100%">
-                            <el-table-column type="expand" label=""></el-table-column>
-                            <el-table-column type="index" label="#"></el-table-column>
-                            <el-table-column prop="paramsName" label="参数名称"></el-table-column>
-                            <el-table-column prop="path" label="操作">
-                                <template slot-scope="scope">
-                                    <el-button type="primary" icon="el-icon-edit" size="mini" @click='edit_roleMethods(scope.row)'>编辑</el-button>
-                                    <el-button type="danger" icon="el-icon-delete" size="mini" @click='del_role(scope.row)'>删除</el-button>
-                                    <el-tooltip content="分配角色" placement="top" :enterable="false">
-                                        <el-button type="warning" icon="el-icon-setting" size="mini" @click='setRights(scope.row)'>分配权限</el-button>
-                                    </el-tooltip>
-                                </template>
-                            </el-table-column>
-                        </el-table>
-                    </template>
+            <el-tabs :tab-position="tabPosition" style="height: 200px;" v-model="activeTabsName" @tab-click='handleTabCliclk()'>
+                <!-- 动态参数 -->
+                <el-tab-pane label="动态参数" name="many">
+                    <el-button type="primary" class='addCatg' @click='addParamsBtn' style='margin:5px 0 20px 0' :disabled="isDisabled">添加参数</el-button>
+                    <el-table :data="manyParamsList" border stripe style="width: 100%">
+                        <el-table-column type="expand" label="">
+                            <template slot-scope='scope'>
+                                <el-tag closable v-for="(i,index) in scope.row.attr_vals" :key='i.attr_id'  @close='delTag(scope.row,index)' :disable-transitions="true">{{ i }}</el-tag>
+                                <el-input
+                                    class="input-new-tag"
+                                    v-if="scope.row.inputVisible"
+                                    v-model="scope.row.inputValue"
+                                    ref="saveTagInput"
+                                    size="small"
+                                    @keyup.enter.native="handleInputConfirm(scope.row)"
+                                    @blur="handleInputConfirm(scope.row)"
+                                    >
+                                </el-input>
+                                <el-button v-else class="button-new-tag" size="small" @click="showInput(scope.row)">+ New Tag</el-button>
+                            </template>
+                        </el-table-column>
+                        <el-table-column type="index" label="#"></el-table-column>
+                        <el-table-column prop="attr_name" label="参数名称"></el-table-column>
+                        <el-table-column prop="path" label="操作">
+                            <template slot-scope="scope">
+                                <el-button type="primary" icon="el-icon-edit" size="mini" @click='editParamsMethods(scope.row)'>修改</el-button>
+                                <el-button type="danger" icon="el-icon-delete" size="mini" @click='del_params(scope.row)'>删除</el-button>
+                            </template>
+                        </el-table-column>
+                    </el-table>
                 </el-tab-pane>
-                <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+                <!-- 静态属性 -->
+                <el-tab-pane label="静态属性" name="only">
+                    <el-button type="primary" class='addCatg' @click='addParamsBtn' style='margin:5px 0 20px 0' :disabled="isDisabled">添加属性</el-button>
+                    <el-table :data="onlyParamsList" border stripe style="width: 100%">
+                        <el-table-column type="expand" label="">
+                            <template slot-scope='scope'>
+                                <el-tag v-for="i in scope.row.attr_vals" :key='i.attr_id' :disable-transitions="false" @close='delTag(scope.row)' closable>{{ i }}</el-tag>
+                                <el-input
+                                    class="input-new-tag"
+                                    v-if="scope.row.inputVisible"
+                                    v-model="scope.row.inputValue"
+                                    ref="saveTagInput"
+                                    size="small"
+                                    @keyup.enter.native="handleInputConfirm(scope.row)"
+                                    @blur="handleInputConfirm(scope.row)"
+                                    >
+                                </el-input>
+                                <el-button v-else class="button-new-tag" size="small" @click="showInput(scope.row)">+ New Tag</el-button>
+                            </template>
+                        </el-table-column>
+                        <el-table-column type="index" label="#"></el-table-column>
+                        <el-table-column prop="attr_name" label="参数名称"></el-table-column>
+                        <el-table-column prop="path" label="操作">
+                            <template slot-scope="scope">
+                                <el-button type="primary" icon="el-icon-edit" size="mini" @click='editParamsMethods(scope.row)'>修改</el-button>
+                                <el-button type="danger" icon="el-icon-delete" size="mini" @click='del_params(scope.row)'>删除</el-button>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </el-tab-pane>
             </el-tabs>
         </el-card>
+        <!-- 添加参数和属性的对话框 -->
+        <el-dialog :title="this.activeTabsName == 'many'?'添加动态参数':'添加静态属性'" :visible.sync="addParamsFormVisible" width="30%" :center="true" @close="addParamsDialogCloseHandle">
+            <el-form :model="addParams" :rules='addParamsFormRules' ref="addParamsForm">
+                <el-form-item :label="this.activeTabsName == 'many'?'参数名称':'静态属性'" prop='attr_name' :label-width="formLabelWidth">
+                    <el-input v-model="addParams.attr_name" autocomplete="off" clearable></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="addParamsFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="addParamsConfirm">确 定</el-button>
+            </div>
+        </el-dialog>
+        <!-- 修改参数和属性的对话框 -->
+        <el-dialog :title="this.activeTabsName == 'many'?'修改动态参数':'修改静态属性'" :visible.sync="editParamsFormVisible" width="30%" :center="true" @close="editParamsDialogCloseHandle">
+            <el-form :model="editParams" :rules='addParamsFormRules' ref="editParamsForm">
+                <el-form-item :label="this.activeTabsName == 'many'?'参数名称':'静态属性'" prop='attr_name' :label-width="formLabelWidth">
+                    <el-input v-model="editParams.attr_name" clearable></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="editParamsFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="editParamsConfirm">确 定</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -55,7 +124,22 @@ export default {
             tabPosition: 'top',
             catgList:[],
             selected_options:[],
-            paramsList:[]
+            manyParamsList:[],
+            formLabelWidth:'80px',
+            onlyParamsList:[],
+            isDisabled:true,
+            activeTabsName:'many',
+            addParams:{},
+            editParams:{
+                attr_name:''
+            },
+            addParamsFormVisible:false,
+            editParamsFormVisible:false,
+            addParamsFormRules:{
+                attr_name:[{
+                    required:true,trigger:'blur',message:'请输入参数名称！'
+                }]
+            }
         }
     },
     methods:{
@@ -69,11 +153,152 @@ export default {
                 this.$message.error({ message: res.meta.msg, center: true });
             }else{                
                 this.catgList = res.data
-                this.total = res.data.total
             }
         },
-        handleChange(){
-
+        handleChange(v){
+            if(v.length != 3){
+                this.isDisabled = true
+                this.selected_options = []
+                this.manyParamsList = []
+                this.onlyParamsList = []
+                return
+            }else{
+                this.isDisabled = false
+                this.getCatgAttr()
+            }
+        },
+        addParamsBtn(){
+            this.addParamsFormVisible = true
+        },
+        handleTabCliclk(){
+            this.getCatgAttr()
+        },
+        async getCatgAttr(){
+            const {data : res} =await this.$http.get(`categories/${this.selected_options[2]}/attributes`,{
+                params:{
+                    sel:this.activeTabsName
+                }
+            })
+            if(res.meta.status != 200){
+                this.$message.error({
+                    message:res.meta.msg,
+                    center:true
+                })
+            }else{
+                res.data.forEach(i=>{
+                    if(i.attr_vals){
+                        i.attr_vals = i.attr_vals.split(',')
+                    }else{
+                        i.attr_vals = []
+                    }
+                    i.inputValue = ''
+                    i.inputVisible = false
+                })
+                if(this.activeTabsName === 'many'){
+                    this.manyParamsList = res.data                    
+                }else{
+                    this.onlyParamsList = res.data  
+                }
+            }
+        },
+        addParamsDialogCloseHandle(){
+            this.$refs.addParamsForm.resetFields()
+        },
+        async addParamsConfirm(){
+            const{data : res} = await this.$http.post(`categories/${this.selected_options[2]}/attributes`,{
+                attr_name:this.addParams.attr_name,
+                attr_sel:this.activeTabsName
+            })
+            if(res.meta.status != 201){
+                this.$message.error({ message: res.meta.msg, center: true });
+            }else{
+                console.log(res)
+                 this.$message.success({ message: res.meta.msg, center: true });
+                 this.getCatgAttr()
+                 this.addParamsFormVisible = false
+            }            
+        },
+        editParamsMethods(scope){
+            this.editParamsFormVisible = true
+            this.editParams.attr_name = scope.attr_name
+            this.editParams.attr_id = scope.attr_id
+        },
+        async editParamsConfirm(){
+            const { data : res } = await this.$http.put(`categories/${this.selected_options[2]}/attributes/${ this.editParams.attr_id }`,{
+                attr_name:this.editParams.attr_name,
+                attr_sel:this.activeTabsName
+            })
+            if(res.meta.status !== 200){
+                this.$message.error({message:res.meta.msg,center:true})  
+            }else{
+                this.$message.success({message:res.meta.msg,center:true})
+                this.getCatgAttr()
+                this.editParamsFormVisible = false
+            }
+        },
+        editParamsDialogCloseHandle(){
+            this.$refs.editParamsForm.resetFields()
+        },
+        // 删除分类
+        del_params(scope){
+            this.$confirm('确定删除该分类？操作将不可撤销！','提示',{
+                type:'warning'
+            }).then(async ()=>{
+                const { data : res } = await this.$http.delete(`categories/${this.selected_options[2]}/attributes/${scope.attr_id}`)
+                if(res.meta.status !== 200){
+                    this.$message.error({message:res.meta.msg,center:true})  
+                }else{
+                    this.$message.success({message:res.meta.msg,center:true})
+                    this.getCatgAttr()
+                }
+            }).catch(()=>{
+                this.$message.info({message:"取消删除",center:true})
+            })
+        },
+        showInput(scope) {
+            scope.inputVisible = true;
+            this.$nextTick(()=> {
+                this.$refs.saveTagInput.$refs.input.focus();
+            });
+        },
+        async handleInputConfirm(scope) {
+            let inputValue = scope.inputValue;
+            if (inputValue.trim()) {
+                const {data : res} = await this.$http.put(`categories/${this.selected_options[2]}/attributes/${scope.attr_id}`,{
+                    attr_name:scope.attr_name,
+                    attr_vals:scope.attr_vals.join(',').concat(',' + inputValue),
+                    attr_sel:this.activeTabsName
+                })
+                if(res.meta.status != 200){
+                    this.$message.error({message:res.meta.msg,center:true})  
+                }else{
+                    scope.attr_vals.push(inputValue)
+                    this.$message.success({message:res.meta.msg,center:true})
+                }
+            }
+            scope.inputVisible = false;
+            scope.inputValue = '';
+        },
+        delTag(scope,index){
+            this.$confirm('确定删除该标签？操作将不可撤销！','提示',{
+                type:'warning'
+            }).then(async ()=>{
+                var spliceItems = scope.attr_vals.splice(index,1)
+                const { data : res } = await this.$http.put(`categories/${this.selected_options[2]}/attributes/${scope.attr_id}`,{
+                    attr_name:scope.attr_name,
+                    attr_vals:scope.attr_vals.join(','),
+                    attr_sel:this.activeTabsName
+                })
+                if(res.meta.status !== 200){
+                    scope.attr_vals.splice(index,0,spliceItems.join(''))
+                    return this.$message.error({message:res.meta.msg,center:true})
+                }else{
+                    this.$message.success({message:res.meta.msg,center:true})
+                }
+                console.log(res)
+            }).catch(()=>{
+                this.$message.info({message:"取消删除",center:true})
+            })            
         }
     },
     mounted(){
@@ -89,4 +314,16 @@ export default {
 .el-card{
     padding-bottom: 30px;
 }
+.el-tabs{
+    height: auto !important;
+}
+.el-tag{
+    margin:6px;
+    cursor: pointer;
+}
+.input-new-tag {
+    width: 90px;
+    margin-left: -1px;
+    vertical-align: middle;
+  }
 </style>
